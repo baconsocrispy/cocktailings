360cd867f2c26d3ce888fd3c421903e58b9fedef
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-10-21 09:30:58 -0700

    Added recipe seeding:

---
+++ db/seed_recipe.rb
@@ -0,0 +1,65 @@
+def seed_recipes(count, text, max_name_length)
+  until Recipe.count == count   
+    recipe = Recipe.new
+    ingredients = seed_recipe_ingredients(recipe)
+    update_recipe_name(recipe, text, max_name_length)
+    recipe.save!
+  end
+end
+
+# --------------- RECIPE SEEDING HELPERS ---------------- #
+def seed_recipe_ingredients(recipe)
+  ingredients = [*seed_ingredient_type(Spirit, 1),
+                 *seed_ingredient_type(Garnish, 2),
+                 *seed_ingredient_type(Modifier, 2),
+                 *seed_ingredient_type(Sugar, 1)]
+
+  ingredients.each { |i| recipe.portions.build(ingredient_id: i.id) }
+end
+
+def seed_ingredient_type(type, quantity)
+  s = []
+  quantity.times do
+    num = rand(0...type.all.length)
+    s << type.all[num] 
+  end
+  return s
+end
+
+def update_recipe_name(recipe, text, max_length)
+  words = get_random_words(text, max_length)
+  name = make_name(words)
+  recipe.name = name
+end
+
+def make_name(words)
+  separators = [' ', ' & ', ' of the ', ' and ', ' with ']
+  length = rand(2..words.length)
+  name_words = words[0...length]
+  name_words.count == 2 ? 
+    name = name_words.join(separators[rand(0...separators.length)]) :
+    name = name_words.join(' ')
+  return name.titleize
+end
+
+def get_random_words(text, count)
+  rejected_words = [ 'and', 'the', 'did', 'with', 'for', 'their', 'they', 'him', 'her', 'went', 'had', 'shall', 'when' ]
+  words = []
+  count.times do
+    word = ''
+    until word.length >= 3 && !rejected_words.include?(word)
+      num = rand(0...text.count)
+      unclean = text[num]
+      word = clean_word(unclean)
+    end
+    words << word
+  end
+  return words
+end
+
+def clean_word(word)
+  rejected_chars = ['.', '"\"', ',', '!', '"', '-', '?', ';']
+  clean_word = ''
+  word.each_char { |c| clean_word += c unless rejected_chars.include?(c) }
+  return clean_word
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,110 +7,58 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 require 'csv'
+require Rails.root.join('db', 'seed_recipe.rb')
 
 # seed the database with ingredients 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'ingredients.csv'))
-# csv = CSV.parse(csv_text, :headers => true)
-# csv.each do |row|
-#   ingredient = Ingredient.where(:display_name => row['display_name'])
-#   if !ingredient.empty?
-#     ingredient.update!(type: row[0],
-#                       display_name: row['display_name'],
-#                       sub_type: row['sub_type'],
-#                       brand: row['brand'],
-#                       product: row['product'],
-#                       abv: row['abv'],
-#                       age: row['age'])
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'ingredients.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  ingredient = Ingredient.where(:display_name => row['display_name'])
+  if !ingredient.empty?
+    ingredient.update!(type: row[0],
+                      display_name: row['display_name'],
+                      sub_type: row['sub_type'],
+                      brand: row['brand'],
+                      product: row['product'],
+                      abv: row['abv'],
+                      age: row['age'])
   
-#   else
-#     Ingredient.create!(type: row[0],
-#                       display_name: row['display_name'],
-#                       sub_type: row['sub_type'],
-#                       brand: row['brand'],
-#                       product: row['product'],
-#                       abv: row['abv'],
-#                       age: row['age'])
-#   end
-# end
+  else
+    Ingredient.create!(type: row[0],
+                      display_name: row['display_name'],
+                      sub_type: row['sub_type'],
+                      brand: row['brand'],
+                      product: row['product'],
+                      abv: row['abv'],
+                      age: row['age'])
+  end
+end
 
-# universal_cabinet = Cabinet.find_by(:name => 'Universal Cabinet')
+universal_cabinet = Cabinet.find_by(:name => 'Universal Cabinet')
 
-# if universal_cabinet
-#   universal_cabinet.portions.destroy_all
-#   Ingredient.all.each do |i|
-#     portion = Portion.create!(ingredient_id: i.id,
-#                         portionable_type: 'Cabinet',
-#                         portionable_id: universal_cabinet.id)
-#     universal_cabinet.portions << portion
-#   end
-# else
-#   universal_cabinet = Cabinet.create!(name: 'Universal Cabinet')
-#   Ingredient.all.each do |i|
-#     portion = Portion.create!(ingredient_id: i.id,
-#                         portionable_type: 'Cabinet',
-#                         portionable_id: universal_cabinet.id)
-#     universal_cabinet.portions << portion
-#   end
-#   universal_cabinet.save!
-# end
+if universal_cabinet
+  universal_cabinet.portions.destroy_all
+  Ingredient.all.each do |i|
+    portion = Portion.create!(ingredient_id: i.id,
+                        portionable_type: 'Cabinet',
+                        portionable_id: universal_cabinet.id)
+    universal_cabinet.portions << portion
+  end
+else
+  universal_cabinet = Cabinet.create!(name: 'Universal Cabinet')
+  Ingredient.all.each do |i|
+    portion = Portion.create!(ingredient_id: i.id,
+                        portionable_type: 'Cabinet',
+                        portionable_id: universal_cabinet.id)
+    universal_cabinet.portions << portion
+  end
+  universal_cabinet.save!
+end
 
 text = File.read(Rails.root.join('lib', 'seeds', 'iliad.txt'))
 iliad = text.split(' ')
 
-Recipe.destroy_all
+seed_recipes(2000, iliad, 3)
 
-1000.times do 
-  r = Recipe.new
-  i = []
 
-  1.times do 
-    num = rand(0...Spirit.all.length)
-    i << Spirit.all[num]
-  end
 
-  5.times do
-    num = rand(0...Ingredient.all.length)
-    i << Ingredient.all[num]
-  end
-
-  i.each do |ing|
-    r.portions.build(ingredient_id: ing.id)
-  end
-  
-  words = ['']
-  3.times do
-    word = ''
-    until word.length > 3
-      num = rand(0...iliad.count)
-      word = iliad[num]
-    end
-    words << word
-  end
-
-  separators = [' ', ' & ', ' of the ', ' and ', ' a la ']
-  rand_words = []
-  count = 0
-  until rand_words.count == 3
-    num = rand(0...words.count)
-    word = words[num]
-    if word.empty? && count == 0
-      rand_words << word
-      count += 1
-    elsif !word.empty?
-      rand_words << word
-    end
-  end
-  rand_words.reject! { |w| w.empty? }
-
-  name = ''
-
-  rand_words.count == 2 ? 
-    name = rand_words.join(separators[rand(0...separators.length)]) :
-    name = rand_words.join(' ')
-
-  r.name = name.titleize
-  p r.name
-  r.save!
-end
-
-p 'SEEDS SUCCESSFULLY RUN'
