ac3fba4c0f6c6ff4aff27ca64e1979f3f67d5910
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-11-09 11:52:03 -0800

    Added check-mark toggling for selected category"

--- app/javascript/controllers/categories_controller.js
+++ app/javascript/controllers/categories_controller.js
@@ -3,7 +3,19 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     console.log("Categories Controller Loaded");
+
+    // toggles the selected category check-mark
+    $('.category-option').on('click', ({ target }) => {
+      if ($('#category-check')) {
+        $('#category-check').closest('.category-option').data('isSelected', false);
+        $('#category-check').remove();
+      }  
+      const html = "<span id='category-check'>&check;</span>";
+      $(this).data('isSelected', true);
+      target.insertAdjacentHTML('afterbegin', html);
+    });
   }
+
 
   toggleCategoryMenu() {
     const menu = $('.category-toggle');
