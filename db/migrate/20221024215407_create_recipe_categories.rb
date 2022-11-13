707274c9e78cbfd4fe5098271cd3bf1d676e9e61
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-11-12 16:53:11 -0800

    fixing migrations

--- db/migrate/20221024215407_create_recipe_categories.rb
+++
@@ -1,8 +0,0 @@
-class CreateRecipeCategories < ActiveRecord::Migration[7.0]
-  def change
-    create_table :recipe_categories do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20221024230652_add_columns_to_recipes_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToRecipesCategories < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :recipe_categories
-    
-  end
-end
