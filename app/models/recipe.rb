902c71f6d9134bd8f51f1a06679532b6dd665372
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-11-17 09:52:59 -0800

    added filtering scopes to recipe model and then refactored search method into one liner switch statements

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,104 +10,56 @@ class Recipe < ApplicationRecord
   has_many :favorite_recipes
   has_many :favorited_by, through: :favorite_recipes, source: :user
 
-  # add most_recent, popular (favorited?/viewed?), highest_rated, difficulty
-  scope :alphabetical, -> { order(:name) }
-  scope :by_category, lambda{ |category_ids| self.joins(:categories).where(categories: { id: category_ids }) if category_ids.present? }
-  scope :by_ingredient, -> (ingredient_ids) { joins(:ingredients).where(ingredients: { id: ingredient_ids })}
-  scope :by_category_and_ingredient, -> (category_ids, ingredient_ids) { by_category(category_ids).by_ingredient(ingredient_ids).distinct }
-  scope :search2, -> (query) { where('recipes.name ILIKE ?', "%#{ query }%").or(where('ingredients.display_name ILIKE ?', "%#{ query }%")) }
-
   accepts_nested_attributes_for :steps, allow_destroy: true
   accepts_nested_attributes_for :categories_recipes, allow_destroy: false
   accepts_nested_attributes_for :portions, allow_destroy: true, reject_if: proc { |att| att['ingredient_id'].blank? }
 
   validates :name, presence: true
 
-  # -------------- RECIPE SEARCH / FILTERING LOGIC --------------- #
+  # ordering scopes
+  scope :alphabetical, -> { order(:name) }
+
+  # filtering scopes
+  scope :search, lambda{ |search_term| self.joins(:ingredients).where('ingredients.display_name ILIKE ?', "%#{ search_term }%").or(where('recipes.name ILIKE ?', "%#{ search_term }%")).distinct if search_term.present?}
+  scope :by_category, lambda{ |category_ids| self.joins(:categories).where(categories: { id: category_ids }) if category_ids.present? }
+  scope :by_all_ingredients, lambda{ |ingredient_ids| self.joins(:ingredients).group(:id).having('array_agg(ingredients.id) @> ?', ingredient_ids) if ingredient_ids.present? }
+  scope :by_any_ingredient, lambda{ |ingredient_ids| self.joins(:ingredients).where(ingredients: { id: ingredient_ids }).distinct }
+  scope :user_has_all_ingredients, lambda{ |user_ingredients| self.joins(:ingredients).group(:id).having('array_agg(ingredients.id) <@ ?', user_ingredients) if user_ingredients.present? }
 
   def self.search_recipes(search_params, user)
     sort_option = search_params[:sortOption]
     category_ids = search_params[:categoryIds]
-    ingredient_ids = search_params[:ingredientIds]
     search_term = search_params[:searchTerm]
+
+    # needed to cast these into Postgres array format in order to function properly
+    ingredient_ids = '{' + search_params[:ingredientIds].join(', ') + '}' if search_params[:ingredientIds]
+    user_ingredients = '{' + user.ingredients.join(', ') + '}'
 
     case sort_option
     when 'All Recipes'
-      ingredient_ids ? 
-        match_ingredients(category_ids, ingredient_ids)
-          .search(search_term) :
-        by_category(category_ids)
-          .search(search_term)
-    when 'I Have Any Ingredient'
-      ingredient_ids ?
-        match_any_subset(category_ids, user.ingredients, ingredient_ids)
-          .search(search_term) :
-        by_category_and_ingredient(category_ids, user.ingredients)
-          .search(search_term)
+      by_category(category_ids)
+        .by_all_ingredients(ingredient_ids)
+        .search(search_term)
+    when 'I Have Any Ingredient' 
+      by_category(category_ids)
+        .by_any_ingredient(user.ingredients)
+        .by_all_ingredients(ingredient_ids)
+        .search(search_term)
     when 'I Have All Ingredients'
-      ingredient_ids ?
-        match_all_subset(category_ids, user.ingredients, ingredient_ids)
-          .search(search_term) :
-        user_has_all_ingredients(category_ids, user.ingredients)
-          .search(search_term)
+      by_category(category_ids)
+        .user_has_all_ingredients(user_ingredients)
+        .by_all_ingredients(ingredient_ids)
+        .search(search_term)
     when 'Favorites'
-      ingredient_ids ?
-        user.favorites.match_ingredients(category_ids, ingredient_ids)
-          .search(search_term) :
-        user.favorites.by_category(category_ids)
-          .search(search_term)
+      user.favorites
+        .by_category(category_ids)
+        .by_all_ingredients(ingredient_ids)
+        .search(search_term)
     when 'My Recipes'
-      ingredient_ids ?
-        user.recipes.match_ingredients(category_ids, ingredient_ids)
-          .search(search_term) :
-        user.recipes.by_category(category_ids)
-          .search(search_term)
+      user.recipes
+        .by_category(category_ids)
+        .by_all_ingredients(ingredient_ids)
+        .search(search_term)
     end
-  end
-
-  def self.search(query)
-    joins(:ingredients).scoping do
-      where('ingredients.display_name ILIKE ?', "%#{ query }%")
-      .or(where('recipes.name ILIKE ?', "%#{ query }%"))
-      .distinct
-    end
-  end
-
-  # returns the set of recipes from all recipes that can be
-  # made from any of the ingredients in the array
-  def self.match_ingredients(category_ids, ingredient_ids)
-    ingredient_ids = '{' + ingredient_ids.join(', ') + '}'
-    by_category(category_ids)
-      .joins(:ingredients)
-      .group(:id)
-      .having('array_agg(ingredients.id) @> ?', ingredient_ids)
-  end
-
-  # returns all recipes that match ANY of the user's ingredients and then 
-  # filters those down to those matching ingredients selected from the cabinet
-  def self.match_any_subset(category_ids, primary_ingredient_ids, subset_ids)
-    subset_ids = '{' + subset_ids.join(', ') + '}'
-    by_category_and_ingredient(category_ids, primary_ingredient_ids)
-      .group(:id)
-      .having('array_agg(ingredients.id) @> ?', subset_ids)
-  end
-
-  # returns all recipes that the user has all the ingredients for
-  def self.user_has_all_ingredients(category_ids, user_ingredients)
-    # casts user_ingredients to postgres array syntax
-    user_ingredients = '{' + user_ingredients.join(', ') + '}'
-    by_category(category_ids)
-      .joins(:ingredients)
-      .group(:id)
-      .having('array_agg(ingredients.id) <@ ?', user_ingredients)
-  end
-
-  # returns all recipes that the user has ALL the ingredients for and then
-  # filters those down to those matching ingredients selected from the cabinet
-  def self.match_all_subset(category_ids, primary_ingredient_ids, subset_ids)
-    subset_ids = '{' + subset_ids.join(', ') + '}'
-    user_has_all_ingredients(category_ids, primary_ingredient_ids)
-      .group(:id)
-      .having('array_agg(ingredients.id) @> ?', subset_ids)
   end
 end
