06af8440b3ae35c8710a03bf13abe30c6d9b0a46
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-11-28 15:07:17 -0800

    added sort menu for mobile media query

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -5,4 +5,19 @@ export default class extends Controller {
     $('.sorting-option').each((_, option) => option.setAttribute('data-isSelected', false));
     target.setAttribute('data-isSelected', true);
   }
+
+  toggleMobileSortMenu() {
+    var isOpen = $('#mobile-sort-menu-container').data('isOpen');
+    $('#mobile-sort-menu-container').data('isOpen', !isOpen);
+  }
+
+  closeSortMenu() {
+    $('body').css('position', 'absolute');
+    $('#mobile-sort-menu-container').hide();
+  }
+
+  openSortMenu() {
+    $('body').css('position', 'fixed');
+    $('#mobile-sort-menu-container').show();
+  }
 }
