ce53cc0f2ae38fc65c256d6e4a6b2984e0a3cfb7
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-11-29 11:27:49 -0800

    fixed bug in current_cabinet helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_cabinet
-    current_user.default_cabinet ? 
+    current_user ? 
       Cabinet.find(current_user.default_cabinet) :
       Cabinet.find_by(name: 'Fully-Stocked Cabinet')
   end
