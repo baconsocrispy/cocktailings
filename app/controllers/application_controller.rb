5a88a40bcff6546a555686e15793b7a6203a0703
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-11-29 10:42:37 -0800

    created current_cabinet helper and implemented in cabinet sidebar and mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
-# since portions can be added to recipes or cabinets
-# this helper function allows portions to be added to either
-# model without duplicating logic. Nested loop shouldn't be
-# a drain on resources since there will never be a large
-# number of portions
-
 class ApplicationController < ActionController::Base
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def current_cabinet
+    current_user.default_cabinet ? 
+      Cabinet.find(current_user.default_cabinet) :
+      Cabinet.find_by(name: 'Fully-Stocked Cabinet')
+  end
 end
